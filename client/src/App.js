77dda33e0ffb1b4714f600d00223e5bbf9384d03
b0nd07 <mhashimkp@gmail.com>
2024-01-19 21:42:40 +0530

    client: add search option

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
+import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
- 
-  <Navbar/>
-  <hr className='border-t border-grey-400'/>
-    </div>
+    <BrowserRouter>
+      <div className="bg-slate-50 min-h-screen">
+        <Navbar />
+        <hr className="border-t border-grey-400" />
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header className='mx-auto px-4 md:px-10 lg:px-20 xl:px-48'>
         <nav className='h-24 flex items-center justify-between'>
             <h3 className='text-red-400 text-3xl font-bold cursor-pointer'>NapBook</h3>
-            <div className='text-red-400 text-lg font-medium cursor-pointer'>
+            <div className='text-red-400 font-medium cursor-pointer'>
                 Sign In
             </div>
           
