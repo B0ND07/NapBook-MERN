d91cfcd7f851484be0bd8f52696320b60e7e7af5
b0nd07 <mhashimkp@gmail.com>
2024-02-17 14:00:29 +0530

    fix: ensure persistence of protected route after page refresh

--- client/src/components/AdminRoute.jsx
+++ client/src/components/AdminRoute.jsx
@@ -1,8 +1,23 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { getUserAction } from "../redux/actions/userActions";
+import Loader from "./Loader";
 
 const AdminRoute = ({ children }) => {
+  const dispatch = useDispatch();
   const { isAuthenticated, user } = useSelector((state) => state.userState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    dispatch(getUserAction())
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
+  }, [dispatch]);
+
+  if (loading) {
+    return <Loader/>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,13 +1,29 @@
-import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { getUserAction } from "../redux/actions/userActions";
+import Loader from "./Loader";
 
 const ProtectedRoute = ({ children }) => {
+  const dispatch = useDispatch();
   const { isAuthenticated } = useSelector((state) => state.userState);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    dispatch(getUserAction())
+      .then(() => setLoading(false))
+      .catch(() => setLoading(false));
+  }, [dispatch]);
+
+  if (loading) {
+    return <Loader/>;
+  }
 
   if (!isAuthenticated) {
     return <Navigate to="/login" />;
   }
-  
+
   return children;
 };
+
 export default ProtectedRoute;
